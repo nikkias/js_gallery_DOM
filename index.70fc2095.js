0b1f591f335e22f4c631ffd702744a3d7ef23776
newqou <137334905+newqou@users.noreply.github.com>
2023-12-02 18:26:56 +0200

    make build

---
+++ index.70fc2095.js
@@ -0,0 +1,4 @@
+var e=document.querySelectorAll("a"),r=document.querySelector("#largeImg").parentNode;// eslint-disable-next-line no-shadow
+e.forEach(function(e){return e.addEventListener("click",function(n){n.preventDefault(),r.innerHTML='\n    <img id="largeImg" src="'.concat(e.href,'"\n    alt="main image" class="gallery__large-img gallery__img">\n  ')})});//# sourceMappingURL=index.70fc2095.js.map
+
+//# sourceMappingURL=index.70fc2095.js.map
